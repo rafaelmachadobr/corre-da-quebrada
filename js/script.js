2df1ae1300107c0daa709e3630ed8da1619823ab
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-03-19 18:35:39 -0300

    Deixando responsivo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+const btnMobile = document.getElementById('btn-mobile');
 const docTitle = document.title;
 
 window.addEventListener("blur", () => {
@@ -6,3 +7,19 @@ window.addEventListener("blur", () => {
 window.addEventListener("focus", () => {
   document.title = docTitle;
 });
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
