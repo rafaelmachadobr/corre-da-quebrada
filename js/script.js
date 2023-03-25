f15e7055dc9cfd0c711487b39f07cfd5eedad831
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-03-25 15:28:31 -0300

    Modal

--- js/script.js
+++ js/script.js
@@ -2,6 +2,20 @@ const btnMobile = document.getElementById("btn-mobile");
 const docTitle = document.title;
 const form = document.querySelector("form");
 
+const openModalButton = document.querySelector("#open-modal");
+const closeModalButton = document.querySelector("#close-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+const toggleModal = () => {
+  modal.classList.toggle("hide");
+  fade.classList.toggle("hide");
+};
+
+[openModalButton, closeModalButton, fade].forEach((el) => {
+  el.addEventListener("click", () => toggleModal());
+});
+
 window.addEventListener("blur", () => {
   document.title = "Volte logo!";
 });
